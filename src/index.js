0222315483ff11216ca8cd004fa2343ab0de4c2c
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-08 12:51:38 +0530

    keep index.js just as the entry point

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import dotenv from "dotenv";
-import express from "express"
+import app from "./app.js";
 dotenv.config({
     path: "./.env"
 })
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/" , (req, res) => {
-    res.send("Welcome to TinyURL Service");
-})
 
 app.listen(port , () => {
     console.log(`Server is running on port http://localhost:${port}`);
