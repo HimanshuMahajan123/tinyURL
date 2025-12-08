ae8630ddc93907e62fa1bb6ddd262ad5e8d3bc39
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-08 13:13:37 +0530

    add CORS support to the application

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
@@ -9,5 +10,15 @@ app.use(express.urlencoded({ extended: true })); // to parse urlencoded data
 app.get('/', (req, res) => {
     res.send('Welcome to the TinyURL Clone API');
 })
+
+//CORS Configurations
+app.use(cors({
+    origin: process.env.CORS_ORIGINS?.split(',') || "http://localhost:5173",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']  
+}))
+
+
 
 export default app;
