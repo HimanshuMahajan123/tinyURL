eca934871b77d10617912d645c0d3d8615d57410
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-08 13:00:44 +0530

    add basic configurations for JSON and URL-encoded data parsing

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+//basic configurations
+app.use(express.json()) // to parse json data
+app.use(express.urlencoded({ extended: true })); // to parse urlencoded data
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the TinyURL Clone API');
+})
+
 export default app;
