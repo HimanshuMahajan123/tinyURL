a457428cedc5bba8aabc0e1b28ba2372e6cfb454
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-08 12:40:35 +0530

    initialize server with Express and dotenv configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+import dotenv from "dotenv";
+import express from "express"
+dotenv.config({
+    path: "./.env"
+})
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/" , (req, res) => {
+    res.send("Welcome to TinyURL Service");
+})
+
+app.listen(port , () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+})
