7b63ee988383d21597832f0f71e56e38e938dae7
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-08 15:52:20 +0530

    add MongoDB connection setup in the application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port , () => {
-    console.log(`Server is running on port http://localhost:${port}`);
-})
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start server:", error);
+  });
